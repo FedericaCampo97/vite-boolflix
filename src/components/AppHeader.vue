4898885c64de6db9ec84354ed43dcd2c17442856
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-28 16:40:12 +0200

    try to make api call

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,3 +1,19 @@
-<script></script>
+<script>
+const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
 <template>
-    <template />
+    <div class="input-group mb-3 mt-4 container">
+        <input type="text" class="form-control" placeholder="Cerca un film" aria-describedby="button-addon1">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon1">VAI</button>
+    </div>
+</template>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,3 +1,26 @@
-<script></script>
-<template>
-    <template />
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            options: {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer 74ec4e13e23b08e73924c0a4a6ae2702'
+                }
+            }
+        }
+    },
+    created() {
+        axios.get('https://api.themoviedb.org/3/authentication', this.options)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(err => console.error(err));
+    }
+}
+</script>
+
+
+<template></template>
