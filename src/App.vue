50630ec1c491a69a810e90fee57b146c64fccbba
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-28 15:37:49 +0200

    Initial exercise

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use './scss/style.scss'
+</style>
