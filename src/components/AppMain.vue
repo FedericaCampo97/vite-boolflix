217239b45e427ed13ad0c772e61684b589dcc600
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-28 18:40:49 +0200

    initial cards film

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,21 @@
 <script>
 import axios from 'axios';
+import cardFilm from './CardFilm.vue';
 export default {
+    components: {
+        cardFilm
+    },
     data() {
         return {
-            options: {
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'Bearer 74ec4e13e23b08e73924c0a4a6ae2702'
-                }
-            }
+            base_url: 'https://api.themoviedb.org/3/search/movie?api_key=74ec4e13e23b08e73924c0a4a6ae2702',
+            films: null
         }
     },
     created() {
-        axios.get('https://api.themoviedb.org/3/authentication', this.options)
+        axios.get(this.base_url + '&query=ritorno+al+futuro')
             .then(response => {
-                console.log(response)
+                this.films = response.data.results
+                console.log(this.films)
             })
             .catch(err => console.error(err));
     }
@@ -23,4 +23,8 @@ export default {
 </script>
 
 
-<template></template>
+<template>
+    <div>
+        <cardFilm v-for="film in films" :film=film></cardFilm>
+    </div>
+</template>
---
+++ src/components/CardFilm.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    props: ['film']
+}
+</script>
+<template>
+    <div>
+        <img src="https://img.illibraio.it/images/9788893675680_92_310_0_75.jpg" alt="">
+        {{ film.title }}
+    </div>
+</template>
